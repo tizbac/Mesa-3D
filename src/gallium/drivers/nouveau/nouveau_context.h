489db481ac9040c43bcb322dfe31101aedbbaa25
Christoph Bumiller <e0425955@student.tuwien.ac.at>
2012-03-01 21:26:50 +0100

    nouveau: add new shared scratch buffers

--- src/gallium/drivers/nouveau/nouveau_context.h
+++ src/gallium/drivers/nouveau/nouveau_context.h
@@ -3,7 +3,11 @@
 
 #include "pipe/p_context.h"
 
+#define NOUVEAU_MAX_SCRATCH_BUFS 4
+
 struct nouveau_pushbuf;
+
+struct nv04_resource;
 
 struct nouveau_context {
    struct pipe_context pipe;
@@ -25,6 +29,19 @@ struct nouveau_context {
                    struct nouveau_bo *, unsigned domain,
                    unsigned base, unsigned size,
                    unsigned offset, unsigned words, const uint32_t *);
+
+   struct {
+      uint8_t *map;
+      unsigned id;
+      unsigned wrap;
+      unsigned offset;
+      unsigned end;
+      struct nouveau_bo *bo[NOUVEAU_MAX_SCRATCH_BUFS];
+      struct nouveau_bo *current;
+      struct nouveau_bo **runout;
+      unsigned nr_runout;
+      unsigned bo_size;
+   } scratch;
 };
 
 static INLINE struct nouveau_context *
@@ -35,5 +52,35 @@ nouveau_context(struct pipe_context *pipe)
 
 void
 nouveau_context_init_vdec(struct nouveau_context *);
+
+/* Copy data to a scratch buffer, update buffer address.
+ * Returns the bo the data resides in, or NULL on failure.
+ */
+struct nouveau_bo *
+nouveau_scratch_data(struct nouveau_context *,
+                     struct nv04_resource *, unsigned base, unsigned size);
+
+/* Release runout buffers. */
+void
+nouveau_scratch_runout_release(struct nouveau_context *);
+
+/* This is needed because we don't hold references outside of context::scratch,
+ * because we don't want to un-bo_ref each allocation every time. This is less
+ * work, and we need the wrap index anyway for extreme situations.
+ */
+static INLINE void
+nouveau_scratch_done(struct nouveau_context *nv)
+{
+   nv->scratch.wrap = nv->scratch.id;
+   if (unlikely(nv->scratch.nr_runout))
+      nouveau_scratch_runout_release(nv);
+}
+
+/* Get pointer to scratch buffer.
+ * The returned nouveau_bo is only referenced by the context, don't un-ref it !
+ */
+void *
+nouveau_scratch_get(struct nouveau_context *, unsigned size, uint64_t *gpu_addr,
+                    struct nouveau_bo **);
 
 #endif
