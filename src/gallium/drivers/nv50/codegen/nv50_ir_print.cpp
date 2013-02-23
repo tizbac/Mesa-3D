429964f74f5970da88425ddcc202b314bc1a7b7e
Christoph Bumiller <e0425955@student.tuwien.ac.at>
2013-02-23 22:33:22 +0100

    MERGE colour

--- src/gallium/drivers/nv50/codegen/nv50_ir_print.cpp
+++ src/gallium/drivers/nv50/codegen/nv50_ir_print.cpp
@@ -531,9 +531,9 @@ void Instruction::print() const
       if (perPatch)
          PRINT("patch ");
       if (asTex())
-         PRINT("%s $r%u $s%u ", asTex()->tex.target.getName(),
-               asTex()->tex.r,
-               asTex()->tex.s);
+         PRINT("%s %s$r%u $s%u %s", asTex()->tex.target.getName(),
+               colour[TXT_MEM], asTex()->tex.r, asTex()->tex.s,
+               colour[TXT_INSN]);
       if (postFactor)
          PRINT("x2^%i ", postFactor);
       PRINT("%s%s", dnz ? "dnz " : (ftz ? "ftz " : ""),  DataTypeStr[dType]);
