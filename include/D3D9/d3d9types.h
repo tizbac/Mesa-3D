2095527b75ae2fd6922b63f2b93d1e04b138f82d
Christoph Bumiller <christoph.bumiller@speed.at>
2013-05-27 00:51:10 +0200

    nine/pipe: don't assert on unknown formats

--- include/D3D9/d3d9types.h
+++ include/D3D9/d3d9types.h
@@ -627,7 +627,12 @@ typedef enum _D3DFORMAT {
     D3DFMT_BINARYBUFFER = 199,
     D3DFMT_DF16 = MAKEFOURCC('D', 'F', '1', '6'),
     D3DFMT_DF24 = MAKEFOURCC('D', 'F', '2', '4'),
-    D3DFMT_NULL = MAKEFOURCC('N', 'U', 'L', 'L')
+    D3DFMT_NULL = MAKEFOURCC('N', 'U', 'L', 'L'),
+    D3DFMT_NV11 = MAKEFOURCC('N', 'V', '1', '1'),
+    D3DFMT_NV12 = MAKEFOURCC('N', 'V', '1', '2'),
+    D3DFMT_Y210 = MAKEFOURCC('Y', '2', '1', '0'),
+    D3DFMT_Y216 = MAKEFOURCC('Y', '2', '1', '6'),
+    D3DFMT_Y410 = MAKEFOURCC('Y', '4', '1', '0')
 } D3DFORMAT;
 
 typedef enum _D3DLIGHTTYPE {
