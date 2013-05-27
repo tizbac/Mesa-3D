1c5633ec578e461a4bf94cdca90a4f3e1e4de861
Christoph Bumiller <christoph.bumiller@speed.at>
2013-05-27 14:48:47 +0200

    gallium/draw: support hack to disable clipping

--- src/gallium/auxiliary/draw/draw_context.c
+++ src/gallium/auxiliary/draw/draw_context.c
@@ -339,6 +339,11 @@ void draw_set_viewport_states( struct draw_context *draw,
        viewport->translate[1] == 0.0f &&
        viewport->translate[2] == 0.0f &&
        viewport->translate[3] == 0.0f);
+
+   draw->driver.bypass_clip_xy = vps[0]->scale[3] == 0.0f;
+   draw->clip_xy = !draw->driver.bypass_clip_xy;
+   draw->identity_viewport =
+      draw->identity_viewport || vpt[0]->scale[3] == 0.0f;
 }
 
 
