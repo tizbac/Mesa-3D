e3d28e2631737b028d1a435328e58e39b2fc6971
Christoph Bumiller <e0425955@student.tuwien.ac.at>
2013-06-11 20:11:10 +0200

    nine/state: set point_size_per_vertex only if VS writes PSIZE

--- src/gallium/state_trackers/nine/nine_pipe.c
+++ src/gallium/state_trackers/nine/nine_pipe.c
@@ -89,7 +89,7 @@ nine_convert_rasterizer_state(struct cso_context *ctx, const DWORD *rs)
  /* rast.point_smooth = 0; */
     rast.sprite_coord_mode = PIPE_SPRITE_COORD_UPPER_LEFT;
     rast.point_quad_rasterization = !!rs[D3DRS_POINTSPRITEENABLE];
-    rast.point_size_per_vertex = 1; /* XXX depends on shader */
+    rast.point_size_per_vertex = rs[NINED3DRS_VSPOINTSIZE];
     rast.multisample = !!rs[D3DRS_MULTISAMPLEANTIALIAS];
     rast.line_smooth = !!rs[D3DRS_ANTIALIASEDLINEENABLE];
  /* rast.line_stipple_enable = 0; */
